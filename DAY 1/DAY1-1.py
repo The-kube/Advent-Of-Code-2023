8dfe652b58c3db7b23a9cd357ce839742cc8f744
TheKube <corradotommasi@hotmail.it>
2023-12-01 16:13:35 +0100

    First challenge

---
+++ DAY 1/DAY1-1.py
@@ -0,0 +1,15 @@
+import re
+
+s = 0
+with open("list.txt", "r") as file:
+    for line in file:
+    # Defining a string with numbers
+
+        temp = re.findall(r'\d', line)
+        if (len(temp) == 1):
+            temp = temp[0] + temp[0]
+        dig = temp[::len(temp)-1]
+        dig = dig[0] + dig[1]
+        s = s + int(dig)
+        print (s)
+print('The total sum is:', s)
