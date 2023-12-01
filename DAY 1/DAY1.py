1d88bdca919c43ffb465172f2e4bbb1ac5c952ca
TheKube <corradotommasi@hotmail.it>
2023-12-01 17:53:33 +0100

    tiding up

--- DAY 1/DAY1.py
+++ DAY 1/DAY1.py
@@ -5,15 +5,15 @@ s = 0
 with open("list.txt", "r") as file:
     for line in file:
         temp = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
-        temp = [element.replace('one', '1')  for element in temp]
-        temp = [element.replace('two', '2')  for element in temp]
-        temp = [element.replace('three', '3')  for element in temp]
-        temp = [element.replace('four', '4')  for element in temp]
-        temp = [element.replace('five', '5')  for element in temp]
-        temp = [element.replace('six', '6')  for element in temp]
-        temp = [element.replace('seven', '7')  for element in temp]
-        temp = [element.replace('eight', '8')  for element in temp]
-        temp = [element.replace('nine', '9')  for element in temp]
+        temp = [element.replace('one',      '1')  for element in temp]
+        temp = [element.replace('two',      '2')  for element in temp]
+        temp = [element.replace('three',    '3')  for element in temp]
+        temp = [element.replace('four',     '4')  for element in temp]
+        temp = [element.replace('five',     '5')  for element in temp]
+        temp = [element.replace('six',      '6')  for element in temp]
+        temp = [element.replace('seven',    '7')  for element in temp]
+        temp = [element.replace('eight',    '8')  for element in temp]
+        temp = [element.replace('nine',     '9')  for element in temp]
 
         print (temp)
         if (len(temp) == 1):
